caa8f41d04f3531d4f53a60073ab0992f5eb87b2
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-09-04 20:53:12 -0300

    Adicionado price-box como uma diretiva

--- src/components/price-box/price-box.directive.js
+++ src/components/price-box/price-box.directive.js
@@ -1,16 +1,5 @@
 "use strict";
 
-angular
-    .module("Website", ["ngRoute"])
-    .config(function($routeProvider, $httpProvider){
-        var templatesPath = "./dist/templates/pages";
-
-        // Routes configuration
-        $routeProvider
-            .when("/", { templateUrl: (templatesPath + "/home/home.template.html") });
-    });; 
-"use strict";
-
 (function() {
 	angular
 		.module("Website")
