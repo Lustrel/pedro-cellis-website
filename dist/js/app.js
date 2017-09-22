60d9cbaea0eecd49dc2baf552648c370fe425a6a
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-09-22 17:44:20 -0300

    .

--- dist/js/app.js
+++ dist/js/app.js
@@ -72,6 +72,43 @@ angular
 "use strict";
 
 (function() {
+    angular
+        .module("Website")
+        .directive("lstMenuButton", Directive);
+
+    function Directive() {
+        return {
+            restrict: "E",
+            templateUrl: "./dist/templates/components/menu-button/menu-button.template.html",
+            transclude: true,
+            replace: true,
+            link: function($scope){
+                console.log($scope);
+                
+                $scope.openMenu = function(){
+                    var $overlay = $("<div></div>");
+                    $overlay.css({
+                        "position": "fixed",
+                        "top": 0,
+                        "left": 0,
+                        "right": 0,
+                        "bottom": 0,
+                        "background": "rgba(0, 0, 0, .8)"
+                    });
+                    
+                    $("body").css({
+                        "overflow": "hidden"
+                    });
+
+                    $("body").append($overlay);
+                };
+            }
+        };
+    }
+})();; 
+"use strict";
+
+(function() {
 	angular
 		.module("Website")
 		.directive("lstPriceBox", Directive);
