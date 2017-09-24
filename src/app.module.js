233895556e630fa45cf5c6d34ce960446f796fe2
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-09-24 15:36:54 -0300

    (1) Atualizado indentacao, de espaco para tab; (2) Feito mais algumas coisas

--- src/app.module.js
+++ src/app.module.js
@@ -1,11 +1,11 @@
 "use strict";
 
 angular
-    .module("Website", ["ngRoute"])
-    .config(function($routeProvider, $httpProvider){
-        var templatesPath = "./dist/templates/pages";
+	.module("Website", ["ngRoute"])
+	.config(function($routeProvider, $httpProvider){
+		var templatesPath = "./dist/templates/pages";
 
-        // Routes configuration
-        $routeProvider
-            .when("/", { templateUrl: (templatesPath + "/home/home.template.html") });
-    });
+		// Routes configuration
+		$routeProvider
+			.when("/", { templateUrl: (templatesPath + "/home/home.template.html") });
+	});
