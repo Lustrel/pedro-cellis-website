585277ea9e3cbd77a0fbe2ee0a8c19c988d42b63
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-09-28 12:52:59 -0300

    Alterado API do Instagram para Pedro Cellis

--- dist/js/app.js
+++ dist/js/app.js
@@ -53,11 +53,13 @@ angular
 			transclude: true,
 			replace: true,
 			link: function(theScope, theElement){
+				// Stanley: 421923937 | 421923937.1677ed0.8b7b578910ea4c08a063c82d882bffb
+				// Pedro Cellis: 291374423 | 291374423.79e9056.3fb796d95fcb491f926058d56a4fb116
 				var feed = new Instafeed({
 					get: "user",
-					userId: "421923937",
+					userId: "291374423",
 					clientId: "79e905681316484b9fe108bbef5774fb",
-					accessToken: "421923937.1677ed0.8b7b578910ea4c08a063c82d882bffba",
+					accessToken: "291374423.79e9056.3fb796d95fcb491f926058d56a4fb116",
 					target: angular.element(theElement)[0],
 					sortBy: "most-recent",
 					limit: 6,
