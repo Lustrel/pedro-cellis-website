d6ce8104fb4fe97943be3650abf53409865662f0
Stanley Sathler <stanleysathlerpinto@gmail.com>
2017-10-05 23:35:13 -0300

    Adicionado cadastro de newsletter

--- dist/js/app.js
+++ dist/js/app.js
@@ -220,4 +220,21 @@ angular
 			callback: $scope.showContactModal
 		}];
 	}
+})();; 
+'use strict';
+
+(function(){
+	angular
+		.module('Website')
+		.controller('SocialController', ['$scope', '$http', Controller]);
+
+	function Controller($scope, $http){
+		$scope.sendNewsletter = function(){
+			$http
+				.post('/php/newsletter.php', { email: $scope.email })
+				.then(function(res){
+					$scope.alreadySent = true;
+				});
+		};
+	}
 })();
